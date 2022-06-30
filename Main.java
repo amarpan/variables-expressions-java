044c21704f8abc8f693607a32a2f06825fda8479
Amar Pan, M.Ed <89368298+profpan396@users.noreply.github.com>
2022-06-30 05:40:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 10;
+
+    double dx = 10.5;
+    double dy = 11.5;
+
+    char c = 'a';
+    boolean bool1 = true;
+  }
+}
